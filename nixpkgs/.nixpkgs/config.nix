144b6928e7ec5964b5b58f95a899ef809411e9f1
Henry Till <henrytill@gmail.com>
2015-02-03 19:10:58 -0500

    nixpkgs: update config.nix

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -1,42 +1,44 @@
 {
   allowUnfree = true;
 
-  packageOverrides = super: let self = super.pkgs; in {
-    inherit (self.stdenv) isDarwin isLinux;
-    inherit (self.stdenv.lib) optionals;
+  packageOverrides = super:
+    let
+      self = super.pkgs;
+      inherit (self.stdenv) isDarwin isLinux;
+      inherit (self.stdenv.lib) optionals;
+    in
+    {
+      baseEnv = self.buildEnv {
+        name = "my-base-environment";
+        paths = with self;
+          # Common Packages
+          [ guile ]
+          # Darwin-specific Packages
+          ++ optionals isDarwin
+          [ coreutils
+            emacs24-nox
+            (gitAndTools.gitFull.override { guiSupport = false; })
+            gnumake
+            gnupg1compat
+            mosh
+            mr
+            offlineimap
+            (pinentry.override { useGtk = false; })
+            stow
+            rsync
+            tmux
+            tree
+            wget
+            xz
+            youtube-dl
+          ]
+          # Linux-specific Packages
+          ++ optionals isLinux
+          [ leiningen
+            pandoc
+          ];
+      };
 
-    myUserPackages = self.buildEnv {
-      name = "my-user-packages";
-      paths = with self;
-        # Common Packages
-        [ guile ]
-        # Darwin-specific Packages
-        ++ optionals isDarwin
-        [ coreutils
-          emacs24-nox
-          (gitAndTools.gitFull.override { guiSupport = false; })
-          gnumake
-          gnupg1compat
-          mosh
-          mr
-          offlineimap
-          (pinentry.override { useGtk = false; })
-          stow
-          rsync
-          tmux
-          tree
-          wget
-          xz
-          youtube-dl
-        ]
-        # Linux-specific Packages
-        ++ optionals isLinux
-        [ leiningen
-          pandoc
-          vicare
-        ];
+      vicare = self.callPackage ./pkgs/vicare { };
     };
-
-    vicare = self.callPackage ./pkgs/vicare { };
-  };
 }
