951af754ad842051d4ac689afd1d305374296e3b
Henry Till <henrytill@gmail.com>
2015-02-04 13:22:07 -0500

    emacs: update darwin-specific config

--- emacs/.emacs.d/ht/keybindings.el
+++ emacs/.emacs.d/ht/keybindings.el
@@ -5,6 +5,3 @@
 (when (file-directory-p "~/Dropbox/doc/org/")
   (global-set-key "\C-cc" 'org-capture)
   (global-set-key "\C-ca" 'org-agenda))
-
-(when (and (is-darwin-p) (window-system))
-  (global-set-key (kbd "C-z") 'ns-do-hide-emacs))
--- emacs/.emacs.d/ht/zism.el
+++ emacs/.emacs.d/ht/zism.el
@@ -2,7 +2,9 @@
 
 ;;; Darwin machines
 (when (and (is-darwin-p) (window-system))
-  (if (file-exists-p (expand-file-name "SourceCodePro-Regular.ttf" "~/Library/Fonts"))
-      (set-default-font "Source Code Pro 13")
-    (set-default-font "Menlo-12"))
-  (invert-face 'default))
+  (when (file-exists-p (expand-file-name "SourceCodePro-Light.ttf" "~/Library/Fonts"))
+    (set-default-font "Source Code Pro Light 13"))
+  (invert-face 'default)
+  (set-frame-position (selected-frame) 0 0)
+  (set-frame-size (selected-frame) 100 48)
+  (server-mode 1))
