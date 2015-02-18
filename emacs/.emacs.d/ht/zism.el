723d4cfd611e45baf40c093b1955b81e6e9aef98
Henry Till <henrytill@gmail.com>
2015-02-17 21:01:24 -0500

    emacs: update darwin-specific config

--- emacs/.emacs.d/ht/zism.el
+++ emacs/.emacs.d/ht/zism.el
@@ -4,11 +4,11 @@
 (when (and (is-darwin-p) (window-system))
   (setq mac-command-modifier 'super
         mac-option-modifier 'meta)
-  (when (file-exists-p (expand-file-name "SourceCodePro-Light.ttf" "~/Library/Fonts"))
-    (set-default-font "Source Code Pro Light 13"))
+  (when (file-exists-p (expand-file-name "mplus-1mn-light.ttf" "~/Library/Fonts"))
+    (set-default-font "M+ 1mn 14"))
   (invert-face 'default)
   (set-frame-position (selected-frame) 0 0)
-  (set-frame-size (selected-frame) 100 48)
+  (set-frame-size (selected-frame) 100 40)
   (setq explicit-shell-file-name (expand-file-name "ansi-term" user-emacs-directory))
   (server-mode 1))
 
