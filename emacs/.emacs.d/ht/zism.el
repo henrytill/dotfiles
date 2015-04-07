af029b0095811f4c2643302dfa2f8dda03c2b774
Henry Till <henrytill@gmail.com>
2015-04-07 16:10:58 -0400

    emacs: update darwin-specific config

--- emacs/.emacs.d/ht/zism.el
+++ emacs/.emacs.d/ht/zism.el
@@ -2,13 +2,18 @@
 
 ;;; Darwin machines
 (when (and (is-darwin-p) (window-system))
-  (setq mac-command-modifier 'super
-        mac-option-modifier 'meta)
-  (when (file-exists-p (expand-file-name "mplus-1mn-regular.ttf" "~/Library/Fonts"))
-    (set-face-attribute 'default nil :font "M+ 1mn 14"))
-  (add-to-list 'default-frame-alist '(height . 40))
-  (add-to-list 'default-frame-alist '(width . 100))
-  (setq explicit-shell-file-name (expand-file-name "ansi-term" user-emacs-directory)))
+  (let ((ansi-term (expand-file-name "ansi-term" user-emacs-directory))
+        (aspell-dir (expand-directory-name "~/.nix-profile/lib/aspell/"))
+        (mplus-font (expand-file-name "mplus-1mn-regular.ttf" "~/Library/Fonts")))
+    (setq explicit-shell-file-name ansi-term
+          mac-command-modifier 'super
+          mac-option-modifier 'meta)
+    (when (file-directory-p aspell-dir)
+      (setenv "ASPELL_CONF" (concat "dict-dir " aspell-dir)))
+    (when (file-exists-p mplus-font)
+      (set-face-attribute 'default nil :font "M+ 1mn 14"))
+    (add-to-list 'default-frame-alist '(height . 40))
+    (add-to-list 'default-frame-alist '(width . 100))))
 
 ;;; NixOS machines
 (when (and (is-linux-p) (file-directory-p "/etc/nixos"))
