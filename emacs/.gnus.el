701cca72fd75605d4792a84c86f9c7257740e5a1
Henry Till <henrytill@gmail.com>
2015-04-23 02:26:14 -0400

    emacs: update gnus config

--- emacs/.gnus.el
+++ emacs/.gnus.el
@@ -1,48 +1,45 @@
-(setq gnus-select-method '(nnnil "")
-      gnus-directory "~/.emacs.d/news/"
-      gnus-home-directory "~/.emacs.d/"
-      gnus-dribble-directory "~/.emacs.d/"
-      gnus-kill-files-directory "~/.emacs.d/news/"
-      gnus-always-dribble-file t
-      user-mail-address "henrytill@gmail.com"
-      user-full-name "Henry Till"
-      gnus-ignored-from-addresses "Henry Till"
+;;;; Gnus
 
-      mail-source-directory "~/.emacs.d/mail"
-      message-directory "~/.emacs.d/mail"
-
-      message-kill-buffer-on-exit t
-      gnus-treat-display-smileys nil
-      gnus-fetch-old-headers 'some
-      nnmail-crosspost nil
-      mail-source-delete-incoming nil
-      gnus-gcc-mark-as-read t
-
-      send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "henrytill@gmail.com" nil))
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service  587
-      
-      gnus-asynchronous t
+(setq gnus-agent-enable-expiration 'DISABLE
       gnus-agent-expire-days 0
       gnus-agent-synchronize-flags t
-      gnus-agent-enable-expiration 'DISABLE
-      gnus-secondary-select-methods 
-      '((nntp "gmane" (nntp-address "news.gmane.org"))
-        (nntp "eternal-september" (nntp-address "news.eternal-september.org"))))
+      gnus-always-dribble-file t
+      gnus-asynchronous t
+      gnus-directory (expand-directory-name "news" user-emacs-directory)
+      gnus-dribble-directory user-emacs-directory
+      gnus-fetch-old-headers 'some
+      gnus-gcc-mark-as-read t
+      gnus-home-directory user-emacs-directory
+      gnus-ignored-from-addresses "Henry Till"
+      gnus-kill-files-directory (expand-directory-name "news" user-emacs-directory)
+      gnus-message-archive-group "sent"
+      gnus-select-method '(nnnil "")
+      gnus-treat-display-smileys nil
+      gnus-use-full-window nil
+      mail-source-delete-incoming nil
+      mail-source-directory (expand-directory-name "mail" user-emacs-directory)
+      message-directory (expand-directory-name "mail" user-emacs-directory)
+      message-kill-buffer-on-exit t
+      nnmail-crosspost nil
+      user-full-name "Henry Till"
+      user-mail-address "henrytill@gmail.com"
+      send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587)
 
-(when (file-directory-p "~/mail/")
-  (add-to-list 'gnus-secondary-select-methods
-               '(nnmaildir "GMail"
-                           (directory "~/mail/henrytill@gmail.com/")
-                           (directory-files nnheader-directory-files-safe)
-                           (get-new-mail nil))))
+(setq gnus-secondary-select-methods
+      '((nnimap "gmail"
+                (nnimap-address "imap.gmail.com")
+                (nnimap-server-port 993)
+                (nnimap-stream ssl))
+        (nnimap "xngns"
+                (nnimap-address "mail.privateemail.com")
+                (nnimap-server-port 993)
+                (nnimap-stream ssl))
+        (nntp "gmane" (nntp-address "news.gmane.org"))
+        (nntp "eternal-september" (nntp-address "news.eternal-september.org"))))
 
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
 (gnus-demon-add-handler 'gnus-group-get-new-news 10 t)
 (gnus-demon-init)
-      
-(setq gnus-use-full-window nil)
