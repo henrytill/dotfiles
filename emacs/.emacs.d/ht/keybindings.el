0068fc04cfe522aa2f64224e946331065c84d717
Henry Till <henrytill@gmail.com>
2015-07-21 18:41:08 -0400

    emacs: add key bindings for hs-minor-mode

--- emacs/.emacs.d/ht/keybindings.el
+++ emacs/.emacs.d/ht/keybindings.el
@@ -7,3 +7,9 @@
 (when (file-directory-p org-directory)
   (global-set-key "\C-cc" 'org-capture)
   (global-set-key "\C-ca" 'org-agenda))
+
+;;; hs-minor-mode
+(require 'hideshow)
+(define-key hs-minor-mode-map (kbd "<f5>")     'hs-toggle-hiding)
+(define-key hs-minor-mode-map (kbd "M-<f5>")   'hs-hide-all)
+(define-key hs-minor-mode-map (kbd "M-S-<f5>") 'hs-show-all)
