56cf551996d63c7e82fd7702ae4b5f32cce478e3
Henry Till <henrytill@gmail.com>
2015-08-21 15:39:49 -0400

    leiningen: update profiles.clj

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,2 @@
-{:user {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]
-        :plugins [[cider/cider-nrepl "0.9.0"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0"]
                   [lein-try "0.4.3"]]}}
