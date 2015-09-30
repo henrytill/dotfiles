1d6ac693b170909cd9315327198a6ff7866a7c70
Henry Till <henrytill@gmail.com>
2015-09-30 18:04:05 -0400

    emacs: update ht-oz-load-path

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -89,7 +89,8 @@
     (cond ((is-darwin-p) "/Applications/Mozart2.app/Contents/Resources")
           ((is-linux-p)  "~/.nix-profile")))
   (defun ht-oz-load-path ()
-    (expand-directory-name "share/mozart/elisp" (ht-oz-home))))
+    (cond ((is-darwin-p) "~/src/other/mozart-elisp")
+          ((is-linux-p)  (expand-directory-name "share/mozart/elisp" (ht-oz-home))))))
 
 
 ;;; packages
