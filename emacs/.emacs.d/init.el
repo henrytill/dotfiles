83ad23e341011e1f883e368af4cea1b9697c9c16
Henry Till <henrytill@gmail.com>
2015-10-31 15:08:31 -0400

    emacs: melpa-stable -> melpa

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -64,7 +64,7 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (when (null package-archive-contents)
