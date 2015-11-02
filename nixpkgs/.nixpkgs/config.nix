6b3c9bead2af9127b996cbb55758b35acb39c98b
Henry Till <henrytill@gmail.com>
2015-11-02 00:18:06 -0500

    nixpkgs: update config.nix

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -1,10 +1,6 @@
 {
   allowUnfree = true;
 
-  firefox = {
-    enableGoogleTalkPlugin = true;
-  };
-
   packageOverrides = super: let self = super.pkgs; in {
     racket-minimal = super.callPackage ./pkgs/racket/minimal.nix {};
   };
