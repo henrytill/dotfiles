72b0cad7495698d152f4a9cf71764a2a619de53a
Henry Till <henrytill@gmail.com>
2016-01-28 20:22:21 -0500

    nixpkgs: add giter8

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -2,6 +2,7 @@
   allowUnfree = true;
 
   packageOverrides = super: let self = super.pkgs; in {
+    giter8 = super.callPackage ./pkgs/giter8 {};
     racket-minimal = super.callPackage ./pkgs/racket/minimal.nix {};
   };
 }
