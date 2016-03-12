bcfff87a21c2c773bfdff90465f0306ca03b8fe1
Henry Till <henrytill@gmail.com>
2016-03-11 23:44:02 -0500

    emacs: update {flycheck,js2-mode} configs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -321,8 +321,8 @@
     :modes scheme-mode
     :predicate ht-rkt-predicate)
   (add-to-list 'flycheck-checkers 'racket-alt)
-  (setq flycheck-completion-system 'ido))
-  (setq-default flycheck-disabled-checkers '(javascript-jslint))
+  (setq flycheck-completion-system 'ido)
+  (setq-default flycheck-disabled-checkers '(javascript-jslint)))
 
 (use-package forth-mode
   :if (executable-find "gforth")
@@ -421,7 +421,15 @@
          ("\\.jsx\\'" . js2-jsx-mode))
   :interpreter ("node" . js2-mode)
   :init
-  (add-hook 'js2-mode-hook 'flycheck-mode)
+  (defun ht-npm-local-executable-path (name)
+    (let* ((npm-bin-path (string-trim (shell-command-to-string "npm bin")))
+           (npm-exe-path (expand-file-name name npm-bin-path)))
+      (when (file-executable-p npm-exe-path) npm-exe-path)))
+  (defun ht-js2-init ()
+    (setq js2-additional-externs '("describe" "it"))
+    (setq flycheck-javascript-eslint-executable (ht-npm-local-executable-path "eslint"))
+    (flycheck-mode 1))
+  (add-hook 'js2-init-hook 'ht-js2-init)
   :config
   (setq js2-basic-offset 2)
   (setq js2-include-node-externs t))
