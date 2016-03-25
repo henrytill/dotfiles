db3bb54469ed837e50446d904b0591dd3730cfbb
Henry Till <henrytill@gmail.com>
2016-03-25 14:02:52 -0400

    nixpkgs: remove .nixpkgs/pkgs

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -1,8 +1,1 @@
-{
-  allowUnfree = true;
-
-  packageOverrides = super: let self = super.pkgs; in {
-    giter8 = super.callPackage ./pkgs/giter8 {};
-    racket-minimal = super.callPackage ./pkgs/racket/minimal.nix {};
-  };
-}
+{ allowUnfree = true; }
