7ac1f379439a12303a772331499ba23e8cc814e0
Henry Till <henrytill@gmail.com>
2016-04-08 09:53:05 -0400

    leiningen: fix cider-nrepl version

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-try "0.4.3"]]}
- :repl {:plugins [[cider/cider-nrepl "0.10.2"]]
+ :repl {:plugins [[cider/cider-nrepl "0.11.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
