0a6ab9ebf83bdc98318e0854b8802647a57c3462
Henry Till <henrytill@gmail.com>
2016-04-18 12:11:44 -0400

    {boot,emacs,leiningen}: use cider{,-nrepl} v0.12.0

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-try "0.4.3"]]}
- :repl {:plugins [[cider/cider-nrepl "0.11.0"]]
+ :repl {:plugins [[cider/cider-nrepl "0.12.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
