95842abab0dee08d35a5d7dc2ce6edf95cacb01d
Henry Till <henrytill@gmail.com>
2016-10-28 20:52:49 -0400

    nixpkgs: update

--- nixpkgs/.nixpkgs/pkgs/htShells.nix
+++ nixpkgs/.nixpkgs/pkgs/htShells.nix
@@ -11,20 +11,21 @@
       name = "shell-jdk7-sbt-bnfc";
       src = null;
       buildInputs = [ myJDK mySbt bnfc ];
-      JAVA_HOME = "${myJDK}/lib/openjdk";
+      JAVA_HOME = "${myJDK}";
     };
 
   jdk7-sbt-maven-bnfc =
     let
       myJDK = jdk7;
       mySbt = sbt.override { jre = myJDK.jre; };
+      myMvn = maven.override { jdk = myJDK; };
       bnfc  = haskellPackages.BNFC;
     in
     stdenv.mkDerivation {
       name = "shell-jdk7-sbt-maven-bnfc";
       src = null;
       buildInputs = [ myJDK mySbt maven bnfc ];
-      JAVA_HOME = "${myJDK}/lib/openjdk";
+      JAVA_HOME = "${myJDK}";
     };
 
   jdk7-sbt-nodejs =
@@ -36,7 +37,7 @@
       name = "shell-jdk7-sbt-nodejs";
       src = null;
       buildInputs = [ myJDK mySbt nodejs ];
-      JAVA_HOME = "${myJDK}/lib/openjdk";
+      JAVA_HOME = "${myJDK}";
     };
 
   jdk8-lein =
@@ -48,7 +49,7 @@
       name = "shell-jdk8-lein";
       src = null;
       buildInputs = [ myJDK myLein ];
-      JAVA_HOME = "${myJDK}/lib/openjdk";
+      JAVA_HOME = "${myJDK}";
     };
 
   jdk8-sbt =
@@ -60,7 +61,7 @@
       name = "shell-jdk8-sbt";
       src = null;
       buildInputs = [ myJDK mySbt ];
-      JAVA_HOME = "${myJDK}/lib/openjdk";
+      JAVA_HOME = "${myJDK}";
     };
 
   jdk8-sbt-nodejs =
@@ -72,6 +73,6 @@
       name = "shell-jdk8-sbt-nodejs";
       src = null;
       buildInputs = [ myJDK mySbt nodejs ];
-      JAVA_HOME = "${myJDK}/lib/openjdk";
+      JAVA_HOME = "${myJDK}";
     };
 }
