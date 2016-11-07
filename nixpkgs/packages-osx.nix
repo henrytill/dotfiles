ad3add5dfe95a939c996aaf346d779abd58fc157
Henry Till <henrytill@gmail.com>
2016-11-07 11:51:12 -0500

    nixpkgs: update packages-osx.nix

--- nixpkgs/packages-osx.nix
+++ nixpkgs/packages-osx.nix
@@ -2,25 +2,12 @@ with import <nixpkgs> {}; [
   aspell
   aspellDicts.en
   cacert
-  ctags
-  (gitAndTools.gitFull.override { guiSupport = false; svnSupport = false; })
-  gnumake
   haskellPackages.cabal-install
   haskellPackages.cabal2nix
   haskellPackages.ghc
   jshon
-  mr
   nix
   nix-repl
-  nodejs
-  offlineimap
-  rlwrap
-  rsync
-  stow
-  tmux
-  tree
-  vim
-  wget
-  xz
-  youtube-dl
+  nixops
+  socat
 ]
