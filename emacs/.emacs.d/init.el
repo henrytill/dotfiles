eb36d5ae1ac35dd1db5e9484778b1a7daccf72df
Henry Till <henrytill@gmail.com>
2016-12-11 22:42:03 -0500

    emacs: update company-coq config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -976,8 +976,12 @@
     :init
     (use-package company-coq
       :ensure t
+      :defines company-coq-disabled-features
       :diminish company-coq-mode
-      :commands company-coq-mode)
+      :commands company-coq-mode
+      :init
+      (setq company-coq-disabled-features '(prettify-symbols
+                                            smart-subscripts)))
     (add-hook 'coq-mode-hook 'company-coq-mode)
     (add-hook 'coq-mode-hook 'electric-pair-mode)
     (add-hook 'coq-mode-hook 'whitespace-mode))
