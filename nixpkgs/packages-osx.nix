67f81b630e8aaa22a983b60794426ae0c382a941
Henry Till <henrytill@gmail.com>
2017-02-14 15:24:55 -0500

    nixpkgs: update packages-osx.nix

--- nixpkgs/packages-osx.nix
+++ nixpkgs/packages-osx.nix
@@ -2,11 +2,16 @@ with import <nixpkgs> {}; [
   aspell
   aspellDicts.en
   cacert
+  haskellPackages.cabal-install
+  haskellPackages.cabal2nix
+  haskellPackages.ghc
+  haskellPackages.idris
+  haskellPackages.purescript_0_10_5
   jshon
   mercurial
   nix
   nix-repl
-  # nixops
+  nixopsUnstable
   offlineimap
   socat
 ]
