d1d34075355dd1eba255873991c7e43bbae7e86b
Henry Till <henrytill@gmail.com>
2017-03-04 01:51:42 -0500

    nixpkgs: update config.nix

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -2,6 +2,17 @@
   allowUnfree = true;
 
   packageOverrides = super: let self = super.pkgs; in {
+
+    haskell = super.haskell // {
+      packages = super.haskell.packages // {
+        ghc802 = super.haskell.packages.ghc802.override {
+          overrides = self: super: {
+            process-extras = self.callPackage ./pkgs/haskell/process-extras-0.7.1.nix {};
+          };
+        };
+      };
+    };
+
     htShells = self.recurseIntoAttrs (self.callPackage ./pkgs/htShells.nix {});
   };
 }
