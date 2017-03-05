79fcbdac04703d46d1c319305c014cffa02a8ae4
Henry Till <henrytill@gmail.com>
2017-03-05 18:02:45 -0500

    nixpkgs: update config.nix

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -3,12 +3,39 @@
 
   packageOverrides = super: let self = super.pkgs; in {
 
-    haskellPackages = super.haskellPackages.override {
-      overrides = self: super: {
-        process-extras = self.callPackage ./pkgs/haskell/process-extras-0.7.1.nix {};
+    haskell =
+      let
+        lib = super.haskell.lib;
+      in
+      super.haskell // {
+        packages = super.haskell.packages // {
+          ghc802 = super.haskell.packages.ghc802.override {
+            overrides = self: super: {
+              process-extras = lib.dontCheck super.process-extras;
+            };
+          };
+        };
       };
-    };
 
-    htShells = self.recurseIntoAttrs (self.callPackage ./pkgs/htShells.nix {});
+    haskellPackages =
+      let
+        lib = super.haskell.lib;
+      in
+      super.haskellPackages.override {
+        overrides = self: super: {
+          purescript = super.purescript.overrideScope (self: super: {
+            aeson                = super.callHackage "aeson"                "0.11.3.0" {};
+            bower-json           = super.callHackage "bower-json"           "1.0.0.1"  {};
+            optparse-applicative = super.callHackage "optparse-applicative" "0.13.1.0" {};
+            http-client          = super.callHackage "http-client"          "0.4.31.2" {};
+            http-client-tls      = super.callHackage "http-client-tls"      "0.2.4.1"  {};
+            pipes                = super.callHackage "pipes"                "4.2.0"    {};
+            websockets           = super.callHackage "websockets"           "0.9.8.2"  {};
+          });
+        };
+      };
+
+    htShells  = super.recurseIntoAttrs (super.callPackage ./pkgs/htShells.nix {});
+    weechat   = super.callPackage ./pkgs/weechat-minimal.nix {};
   };
 }
