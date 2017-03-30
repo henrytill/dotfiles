0591a92ee764fe4db09a87825181dafc6ba9f820
Henry Till <henrytill@gmail.com>
2017-03-29 22:56:10 -0400

    emacs: update feature-scala

--- emacs/.emacs.d/feature/feature-scala.el
+++ emacs/.emacs.d/feature/feature-scala.el
@@ -1,18 +1,19 @@
 (defun ht/scala-mode ()
   (setq scala-indent:align-parameters t))
 
+(defconst ht/flycheck-quickfix-load-path "~/src/flycheck-quickfix")
+
 (use-package scala-mode
   :ensure t
   :mode (("\\.scala\\'" . scala-mode)
          ("\\.sbt\\'"   . scala-mode))
   :init
-  (let ((lp "~/src/flycheck-quickfix"))
-    (use-package flycheck-quickfix
-      :if (lambda () (file-directory-p lp))
-      :load-path lp
-      :config
-      (add-hook 'scala-mode-hook 'flycheck-quickfix-setup)
-      (add-hook 'scala-mode-hook 'flycheck-mode)))
+  (use-package flycheck-quickfix
+    :if (lambda () (file-directory-p ht/flycheck-quickfix-load-path))
+    :load-path ht/flycheck-quickfix-load-path
+    :config
+    (add-hook 'scala-mode-hook 'flycheck-quickfix-setup)
+    (add-hook 'scala-mode-hook 'flycheck-mode))
   (add-hook 'scala-mode-hook 'ht/scala-mode)
   (add-hook 'scala-mode-hook 'auto-revert-mode)
   (add-hook 'scala-mode-hook 'electric-pair-mode))
