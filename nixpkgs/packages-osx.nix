63fca5d7d076409308112b218db51f37f9347237
Henry Till <henrytill@gmail.com>
2017-04-02 13:13:20 -0400

    nixpkgs: refactor packages-osx.nix

--- nixpkgs/packages-osx.nix
+++ nixpkgs/packages-osx.nix
@@ -1,26 +1,28 @@
-with import <nixpkgs> {}; [
-  aspell
-  aspellDicts.en
-  cacert
-  haskellPackages.Agda
-  haskellPackages.cabal-install
-  haskellPackages.cabal2nix
-  haskellPackages.darcs
-  haskellPackages.ghc
-  haskellPackages.idris
-  haskellPackages.purescript
-  irssi
-  jshon
-  mercurial
-  nix
-  nix-prefetch-scripts
-  nix-repl
-  nixopsUnstable
-  offlineimap
-  pandoc
-  socat
-  tree
-  weechat
-  wget
-  youtube-dl
+let
+  pkgs = import <nixpkgs> {};
+in
+[ pkgs.aspell
+  pkgs.aspellDicts.en
+  pkgs.cacert
+  pkgs.haskellPackages.Agda
+  pkgs.haskellPackages.cabal-install
+  pkgs.haskellPackages.cabal2nix
+  pkgs.haskellPackages.darcs
+  pkgs.haskellPackages.ghc
+  pkgs.haskellPackages.idris
+  pkgs.haskellPackages.purescript
+  pkgs.irssi
+  pkgs.jshon
+  # pkgs.mercurial
+  pkgs.nix
+  pkgs.nix-prefetch-scripts
+  pkgs.nix-repl
+  pkgs.nixopsUnstable
+  pkgs.offlineimap
+  pkgs.pandoc
+  pkgs.socat
+  pkgs.tree
+  pkgs.weechat
+  pkgs.wget
+  pkgs.youtube-dl
 ]
