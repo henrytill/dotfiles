cb4e098f6a6cf51e97c9ace19f6c787a84621d45
Henry Till <henrytill@gmail.com>
2017-04-12 14:33:13 -0400

    nixpkgs: update packages-osx.nix

--- nixpkgs/packages-osx.nix
+++ nixpkgs/packages-osx.nix
@@ -1,28 +1,29 @@
 let
-  pkgs = import <nixpkgs> {};
-in
-[ pkgs.aspell
-  pkgs.aspellDicts.en
-  pkgs.cacert
-  pkgs.haskellPackages.Agda
-  pkgs.haskellPackages.cabal-install
-  pkgs.haskellPackages.cabal2nix
-  pkgs.haskellPackages.darcs
-  pkgs.haskellPackages.ghc
-  pkgs.haskellPackages.idris
-  pkgs.haskellPackages.purescript
-  pkgs.irssi
-  pkgs.jshon
-  # pkgs.mercurial
-  pkgs.nix
-  pkgs.nix-prefetch-scripts
-  pkgs.nix-repl
-  pkgs.nixopsUnstable
-  pkgs.offlineimap
-  pkgs.pandoc
-  pkgs.socat
-  pkgs.tree
-  pkgs.weechat
-  pkgs.wget
-  pkgs.youtube-dl
-]
+  nixpkgs = import <nixpkgs> {};
+  pkgs = with nixpkgs;
+    [ aspell
+      aspellDicts.en
+      cacert
+      haskellPackages.Agda
+      haskellPackages.cabal-install
+      haskellPackages.cabal2nix
+      haskellPackages.darcs
+      haskellPackages.ghc
+      haskellPackages.idris
+      haskellPackages.purescript
+      jshon
+      # mercurial
+      msmtp
+      nix
+      nix-prefetch-scripts
+      nix-repl
+      nixopsUnstable
+      notmuch
+      offlineimap
+      pandoc
+      socat
+      tree
+      wget
+      youtube-dl
+    ];
+in pkgs
