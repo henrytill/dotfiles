78925e0b1d80aae047ac36a6471a1fc17beaa159
Henry Till <henrytill@gmail.com>
2017-04-23 20:03:06 -0400

    emacs: update feature-projectile

--- emacs/.emacs.d/feature/feature-projectile.el
+++ emacs/.emacs.d/feature/feature-projectile.el
@@ -10,4 +10,24 @@
   (projectile-register-project-type 'ocamlbuild-script '("_tags" "build") "./build" "./build tests")
   (projectile-register-project-type 'topkg '("_tags" "opam" "pkg") "topkg build" "topkg test"))
 
+(with-eval-after-load 'projectile
+  ;; Redefine to observe projectile-compilation-dir
+  (defun projectile-test-project (arg &optional dir)
+    "Run project test command.
+
+Normally you'll be prompted for a compilation command, unless
+variable `compilation-read-command'.  You can force the prompt
+with a prefix ARG."
+    (interactive "P")
+    (let* ((project-root (projectile-project-root))
+           (default-directory (or dir (projectile-compilation-dir)))
+           (default-cmd (projectile-test-command default-directory))
+           (test-cmd (projectile-maybe-read-command arg default-cmd "Test command: ")))
+      (puthash default-directory test-cmd projectile-test-cmd-map)
+      (save-some-buffers (not compilation-ask-about-save)
+                         (lambda ()
+                           (projectile-project-buffer-p (current-buffer)
+                                                        project-root)))
+      (projectile-run-compilation test-cmd))))
+
 (provide 'feature-projectile)
