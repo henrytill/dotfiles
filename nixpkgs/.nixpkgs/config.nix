e7a73576930642c9a68edb6578094c2c60e9b086
Henry Till <henrytill@gmail.com>
2017-10-16 06:19:11 -0400

    nixpkgs: update config.nix

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -26,14 +26,19 @@
       let
         lib = self.haskell.lib;
         darwinStaticExe = p: if self.stdenv.isDarwin then lib.justStaticExecutables p else p;
+        home = builtins.getEnv "HOME";
+        distProcSimpPath = home + "/src/study-haskell/parconc/distributed-process-simplelocalnet";
       in
       super.haskellPackages.override {
         overrides = self: super: {
-          cabal2nix   = darwinStaticExe super.cabal2nix;
-          darcs       = darwinStaticExe super.darcs;
-          lhs2tex     = darwinStaticExe super.lhs2tex;
-          stack       = darwinStaticExe super.stack;
-          threadscope = darwinStaticExe super.threadscope;
+          cabal2nix                          = darwinStaticExe super.cabal2nix;
+          darcs                              = darwinStaticExe super.darcs;
+          distributed-process-simplelocalnet = super.callPackage distProcSimpPath {};
+          network-transport                  = super.network-transport_0_5_2;
+          network-transport-tcp              = super.network-transport-tcp_0_6_0;
+          lhs2tex                            = darwinStaticExe super.lhs2tex;
+          stack                              = darwinStaticExe super.stack;
+          threadscope                        = darwinStaticExe super.threadscope;
         };
       };
 
