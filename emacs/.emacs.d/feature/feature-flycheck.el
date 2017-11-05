7414eec4c7860f5529add60009d7381e3b40df85
Henry Till <henrytill@gmail.com>
2017-11-05 12:05:46 -0500

    emacs: update feature-flycheck

--- emacs/.emacs.d/feature/feature-flycheck.el
+++ emacs/.emacs.d/feature/feature-flycheck.el
@@ -1,17 +1,24 @@
+
+(use-package flycheck-haskell
+  :ensure t
+  :disabled t
+  :commands flycheck-haskell-setup)
+
+(use-package flycheck-rust
+  :ensure t
+  :commands flycheck-rust-setup)
+
 (use-package flycheck
   :ensure t
   :commands flycheck-mode
   :init
-  (use-package flycheck-haskell
-    :ensure t
-    :commands flycheck-haskell-setup)
-  (use-package flycheck-rust
-    :ensure t
-    :commands flycheck-rust-setup)
   (add-hook 'flycheck-mode-hook 'flycheck-haskell-setup)
   (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
   :config
   (setq flycheck-completion-system 'ido)
-  (setq-default flycheck-disabled-checkers '(javascript-jslint)))
+  (setq-default flycheck-disabled-checkers '(haskell-ghc
+                                             haskell-hlint
+                                             haskell-stack-ghc
+                                             javascript-jslint)))
 
 (provide 'feature-flycheck)
