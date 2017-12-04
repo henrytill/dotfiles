0fa702c1d1fc5159b482fabf7708eedb35fe2158
Henry Till <henrytill@gmail.com>
2017-12-04 15:34:32 -0500

    emacs: update feature-haskell

--- emacs/.emacs.d/feature/feature-haskell.el
+++ emacs/.emacs.d/feature/feature-haskell.el
@@ -7,33 +7,6 @@
   (define-key evil-normal-state-local-map (kbd "C-t") 'xref-pop-marker-stack)
   nil)
 
-(defun ht/haskell-interactive-wrapper (arg)
-  "Prompt user to enter an additional argument to add to haskell-process-args-*"
-  (interactive "sEnter argument: ")
-  (dolist (args '(haskell-process-args-cabal-repl
-                  haskell-process-args-cabal-new-repl
-                  haskell-process-args-ghci
-                  haskell-process-args-stack-ghci))
-    (custom-reevaluate-setting args)
-    (add-to-list args arg))
-  (haskell-interactive-bring))
-
-(defun ht/haskell-toggle-dante-mode ()
-  (interactive)
-  (let ((enable-dante-mode (lambda (x)
-                             (if (bound-and-true-p dante-mode)
-                                 (dante-mode x)
-                               (with-current-buffer (current-buffer)
-                                 (when (eq major-mode 'haskell-mode)
-                                   (dante-mode x)))))))
-    (if (member 'dante-mode haskell-mode-local-vars-hook)
-        (progn
-          (remove-hook 'haskell-mode-local-vars-hook 'dante-mode)
-          (funcall enable-dante-mode 0))
-      (progn
-        (add-hook 'haskell-mode-local-vars-hook 'dante-mode)
-        (funcall enable-dante-mode 1)))))
-
 (use-package dante
   :ensure t
   :defines (dante-project-root
@@ -56,10 +29,12 @@
          ("\\.lhs\\'"     . literate-haskell-mode)
          ("\\.cabal\\'"   . haskell-cabal-mode))
   :init
-  (add-hook 'haskell-mode-local-vars-hook 'dante-mode)
+  (ht/comment
+    (add-hook 'haskell-mode-local-vars-hook 'dante-mode)
+    (add-hook 'haskell-mode-hook 'flycheck-mode)
+    nil)
   (dolist (mode '(company-mode
                   electric-pair-mode
-                  flycheck-mode
                   haskell-indentation-mode
                   ht/haskell-mode
                   interactive-haskell-mode))
