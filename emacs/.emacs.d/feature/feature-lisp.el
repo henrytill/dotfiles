8d5c4ef0563f877bcd3e7a6c8fdc21ecfebdc3b1
Henry Till <henrytill@gmail.com>
2017-12-30 16:08:30 -0500

    emacs: update feature-lisp

--- emacs/.emacs.d/feature/feature-lisp.el
+++ emacs/.emacs.d/feature/feature-lisp.el
@@ -1,10 +1,3 @@
-(defun ht/scheme-mode ()
-  (dolist (form+n '((conde . 0)
-                    (fresh . 1)
-                    (run   . 2)
-                    (run*  . 1)))
-    (put (car form+n) 'scheme-indent-function (cdr form+n))))
-
 (use-package clojure-mode
   :ensure t
   :mode (("\\.clj\\'"  . clojure-mode)
@@ -14,20 +7,13 @@
   (add-to-list 'magic-mode-alist '(".* boot" . clojure-mode))
   (add-hook 'clojure-mode-hook 'enable-paredit-mode))
 
-(use-package ielm
-  :commands ielm
-  :init
-  (add-hook 'ielm-mode-hook 'company-mode)
-  (add-hook 'ielm-mode-hook 'eldoc-mode))
-
 (use-package lisp-mode
   :defer t
   :init
-  (add-hook 'lisp-mode-hook 'eldoc-mode)
-  (add-hook 'lisp-mode-hook 'enable-paredit-mode)
+  (dolist (mode '(eldoc-mode enable-paredit-mode))
+    (add-hook 'lisp-mode-hook mode)
+    (add-hook 'emacs-lisp-mode-hook mode))
   (add-hook 'lisp-interaction-mode-hook 'eldoc-mode)
-  (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
-  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
   (add-to-list 'magic-mode-alist '("#! emacs --script" . emacs-lisp-mode)))
 
 (use-package paren-face
@@ -36,6 +22,13 @@
   :config
   (setq paren-face-regexp "[][(){}]")
   (global-paren-face-mode))
+
+(defun ht/scheme-mode ()
+  (dolist (form+n '((conde . 0)
+                    (fresh . 1)
+                    (run   . 2)
+                    (run*  . 1)))
+    (put (car form+n) 'scheme-indent-function (cdr form+n))))
 
 (use-package scheme
   :mode (("\\.scm\\'" . scheme-mode)
