eb7a2f2133f3ea5b593c1bce6f79e0b3744b05ec
Henry Till <henrytill@gmail.com>
2018-01-14 15:37:41 -0500

    nixpkgs: tweak formatting

--- nixpkgs/.nixpkgs/config.nix
+++ nixpkgs/.nixpkgs/config.nix
@@ -4,49 +4,55 @@
   packageOverrides = super: let self = super.pkgs; in {
 
     haskell =
-      let lib = self.haskell.lib;
-      in super.haskell // {
-        packages = super.haskell.packages // {
-          ghc7103_async_2_0_2 =
-            super.haskell.packages.ghc7103.override {
-              overrides = self: super: {
-                async = super.callHackage "async" "2.0.2" {};
+      let
+        lib = self.haskell.lib;
+      in
+        super.haskell // {
+          packages = super.haskell.packages // {
+            ghc7103_async_2_0_2 =
+              super.haskell.packages.ghc7103.override {
+                overrides = self: super: {
+                  async = super.callHackage "async" "2.0.2" {};
+                };
               };
-            };
-          ghc802_parconc =
-            let dpsSrc = super.pkgs.fetchFromGitHub {
+            ghc802_parconc =
+              let
+                dpsSrc = super.pkgs.fetchFromGitHub {
                            owner           = "haskell-distributed";
                            repo            = "distributed-process-simplelocalnet";
                            rev             = "3e1660fdbd82995ebb05f3c37991597728f622c6";
                            sha256          = "1yg72lksdqwiy80l71z0xrdzjgnmppa29lzfk16bss6rh51xj4jy";
                            fetchSubmodules = true;
                          };
-            in super.haskell.packages.ghc802.override {
-              overrides = self: super: {
-                network-transport                  = super.network-transport_0_5_2;
-                network-transport-tcp              = super.network-transport-tcp_0_6_0;
-                distributed-process-simplelocalnet = super.callCabal2nix "distributed-process-simplelocalnet" dpsSrc {};
-              };
-            };
+              in
+                super.haskell.packages.ghc802.override {
+                  overrides = self: super: {
+                    network-transport                  = super.network-transport_0_5_2;
+                    network-transport-tcp              = super.network-transport-tcp_0_6_0;
+                    distributed-process-simplelocalnet = super.callCabal2nix "distributed-process-simplelocalnet" dpsSrc {};
+                  };
+                };
+          };
         };
-      };
 
     haskellPackages =
-      let darwinStaticExe = p: if self.stdenv.isDarwin then lib.justStaticExecutables p else p;
-          home            = builtins.getEnv "HOME";
-          lib             = self.haskell.lib;
-      in super.haskellPackages.override {
-        overrides = self: super: {
-          Agda            = darwinStaticExe super.Agda;
-          cabal2nix       = darwinStaticExe super.cabal2nix;
-          darcs           = darwinStaticExe super.darcs;
-          idris           = darwinStaticExe super.idris;
-          lhs2tex         = darwinStaticExe super.lhs2tex;
-          stack           = darwinStaticExe super.stack;
-          stylish-haskell = darwinStaticExe super.stylish-haskell;
-          threadscope     = darwinStaticExe super.threadscope;
+      let
+        darwinStaticExe = p: if self.stdenv.isDarwin then lib.justStaticExecutables p else p;
+        home            = builtins.getEnv "HOME";
+        lib             = self.haskell.lib;
+      in
+        super.haskellPackages.override {
+          overrides = self: super: {
+            Agda            = darwinStaticExe super.Agda;
+            cabal2nix       = darwinStaticExe super.cabal2nix;
+            darcs           = darwinStaticExe super.darcs;
+            idris           = darwinStaticExe super.idris;
+            lhs2tex         = darwinStaticExe super.lhs2tex;
+            stack           = darwinStaticExe super.stack;
+            stylish-haskell = darwinStaticExe super.stylish-haskell;
+            threadscope     = darwinStaticExe super.threadscope;
+          };
         };
-      };
 
     ht = {
       packages   = super.callPackage ./pkgs/ht/packages.nix {};
