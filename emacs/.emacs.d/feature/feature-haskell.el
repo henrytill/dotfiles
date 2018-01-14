dc1ac718d20e9169ffc5b1ee5232199ab6366c48
Henry Till <henrytill@gmail.com>
2018-01-14 15:38:37 -0500

    emacs: update feature-haskell

--- emacs/.emacs.d/feature/feature-haskell.el
+++ emacs/.emacs.d/feature/feature-haskell.el
@@ -2,7 +2,8 @@
   (setq electric-indent-local-mode 0
         evil-auto-indent nil
         haskell-doc-prettify-types nil
-        haskell-interactive-popup-errors nil)
+        haskell-interactive-popup-errors nil
+        haskell-process-log t)
   (define-key evil-normal-state-local-map (kbd "C-]") 'xref-find-definitions)
   (define-key evil-normal-state-local-map (kbd "C-t") 'xref-pop-marker-stack)
   nil)
@@ -13,20 +14,6 @@
         (lambda (argv) (append (list "nix-shell" "--command" )
                                (list (mapconcat 'identity argv " "))))))
 
-(use-package dante
-  :ensure t
-  :defines (dante-project-root
-            dante-repl-command-line
-            dante-target)
-  :commands dante-mode)
-
-(use-package company-ghci
-  :ensure t
-  :disabled t
-  :defer t
-  :config
-  (add-to-list 'company-backends 'company-ghci))
-
 (use-package haskell-mode
   :ensure t
   :mode (("\\.hs\\'"      . haskell-mode)
@@ -35,14 +22,40 @@
          ("\\.lhs\\'"     . literate-haskell-mode)
          ("\\.cabal\\'"   . haskell-cabal-mode))
   :init
-  (add-hook 'haskell-mode-local-vars-hook 'dante-mode)
-  (add-hook 'nix-buffer-after-load-hook 'dante-restart)
   (dolist (mode '(company-mode
                   electric-pair-mode
-                  flycheck-mode
                   haskell-indentation-mode
                   ht/haskell-mode
                   interactive-haskell-mode))
     (add-hook 'haskell-mode-hook mode)))
+
+;; https://github.com/haskell/haskell-mode/pull/1573
+(with-eval-after-load 'haskell-load
+  (defun haskell-process-extract-modules (buffer)
+    "Extract the modules from the process buffer. (fixed)"
+    (let* ((modules-string (match-string 1 buffer))
+           (modules (and modules-string (split-string modules-string ", "))))
+      (cons modules modules-string)))
+  nil)
+
+(use-package dante
+  :ensure t
+  :disabled t
+  :defines (dante-project-root
+            dante-repl-command-line
+            dante-target)
+  :commands dante-mode
+  :requires (haskell-mode)
+  :init
+  (add-hook 'haskell-mode-local-vars-hook 'dante-mode)
+  (add-hook 'nix-buffer-after-load-hook 'dante-restart)
+  (add-hook 'haskell-mode-hook 'flycheck-mode))
+
+(use-package company-ghci
+  :ensure t
+  :disabled t
+  :defer t
+  :config
+  (add-to-list 'company-backends 'company-ghci))
 
 (provide 'feature-haskell)
