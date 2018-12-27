b49c2f68e548247ea737e6314d3fe387efc81462
Henry Till <henrytill@gmail.com>
2018-12-27 10:05:40 -0800

    emacs: update c-default-style

--- emacs/.emacs.d/feature/feature-c-cpp.el
+++ emacs/.emacs.d/feature/feature-c-cpp.el
@@ -4,7 +4,7 @@
   (c-add-style "stevens" '("bsd" (c-basic-offset . 4)))
   (c-add-style "hnf"     '("bsd" (c-basic-offset . 2)))
   (c-add-style "hnfcpp"  '("stroustrup" (c-basic-offset . 2)))
-  (add-to-list 'c-default-style '(c-mode . "hnfcpp"))
+  (add-to-list 'c-default-style '(c-mode . "bsd"))
   (add-to-list 'c-default-style '(c++-mode . "hnfcpp")))
 
 (defun ht/c-mode ()
