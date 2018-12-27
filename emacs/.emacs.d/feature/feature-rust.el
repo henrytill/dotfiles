94c0f60ccbb58d4131c819633520e696dc2ddf56
Henry Till <henrytill@gmail.com>
2018-12-27 10:13:03 -0800

    emacs: add mode-specific hydra for rust

--- emacs/.emacs.d/feature/feature-rust.el
+++ emacs/.emacs.d/feature/feature-rust.el
@@ -1,3 +1,11 @@
+(defhydra ht/hydra-rust (:idle 1)
+  "
+rust
+----
+_gd_: racer-find-definition
+"
+  ("gd" racer-find-definition nil :exit t))
+
 (defun ht/racer-mode ()
   (let ((cmd (executable-find "racer")))
     (when cmd
@@ -12,10 +20,14 @@
   (add-hook 'racer-mode-hook 'company-mode)
   (add-hook 'racer-mode-hook 'eldoc-mode))
 
+(defun ht/rust-mode ()
+  (setq-local ht/hydra-mode-specific 'ht/hydra-rust/body))
+
 (use-package rust-mode
   :ensure t
   :mode "\\.rs\\'"
   :init
+  (add-hook 'rust-mode-hook 'ht/rust-mode)
   (add-hook 'rust-mode-hook 'auto-revert-mode)
   (add-hook 'rust-mode-hook 'electric-pair-mode)
   (add-hook 'rust-mode-hook 'flycheck-mode)
