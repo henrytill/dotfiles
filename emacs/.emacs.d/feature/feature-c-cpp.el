940ea1e1dcfd3d7144381cc007cfd579aaf077e6
Henry Till <henrytill@gmail.com>
2019-02-06 16:36:58 -0500

    emacs: update feature-c-cpp, removing rtags config

--- emacs/.emacs.d/feature/feature-c-cpp.el
+++ emacs/.emacs.d/feature/feature-c-cpp.el
@@ -8,72 +8,8 @@
   (add-to-list 'c-default-style '(c-mode . "k&r-4"))
   (add-to-list 'c-default-style '(c++-mode . "hnfcpp")))
 
-(use-package c-mode
-  :mode (("\\.c\\'" . c-mode)
-         ("\\.h\\'" . c-mode))
-  :init
-  (add-hook 'c-mode-hook #'electric-pair-mode))
-
-(use-package c++-mode
-  :mode (("\\.cc\\'"  . c++-mode)
-         ("\\.hh\\'"  . c++-mode)
-         ("\\.cpp\\'" . c++-mode)
-         ("\\.hpp\\'" . c++-mode))
-  :init
-  (add-hook 'c++-mode-hook #'electric-pair-mode))
-
-
-;;; rtags
-
-(eval-and-compile
-  (defun ht/rtags-load-path ()
-    (let ((rdm-executable (executable-find "rdm")))
-      (when rdm-executable
-        (expand-directory-name "../share/emacs/site-lisp/rtags"
-                               (file-name-directory rdm-executable))))))
-
-(defun ht/flycheck-rtags-mode ()
-  (interactive)
-  (flycheck-mode 1)
-  (when (not (featurep 'flycheck-rtags))
-    (require 'flycheck-rtags))
-  (flycheck-select-checker 'rtags)
-  (setq-local flycheck-highlighting-mode nil))
-
-(defhydra ht/hydra-rtags (:idle 1)
-  "
-rtags
------
-_t_: rtags-symbol-type
-"
-  ("t" rtags-symbol-type nil :exit t))
-
-(defun ht/rtags-mode ()
-  (setq rtags-autostart-diagnostics t
-        rtags-completions-enabled t
-        rtags-display-result-backend 'ivy)
-  (setq-local ht/hydra-mode-specific 'ht/hydra-rtags/body)
-  (rtags-start-process-unless-running))
-
-(use-package rtags
-  :if (executable-find "rdm")
-  :defines rtags-start-process-unless-running
-  :commands rtags-start-process-unless-running
-  :load-path (lambda () (ht/rtags-load-path))
-  :init
-  (add-hook 'c-mode-hook   #'ht/rtags-mode)
-  (add-hook 'c++-mode-hook #'ht/rtags-mode)
-  :config
-  (when (not (featurep 'company-rtags))
-    (require 'company-rtags)
-    (add-to-list 'company-backends 'company-rtags))
-  (when (not (featurep 'ivy-rtags))
-    (require 'ivy-rtags))
-  (dolist (map '(c-mode-map c++-mode-map))
-    (dolist (binding '(("C-c C-a" . ff-find-other-file)
-                       ("C-]"     . rtags-find-symbol-at-point)
-                       ("C-t"     . rtags-location-stack-back)))
-      (eval `(evil-define-key 'normal ,map (kbd (car binding)) (cdr binding))))))
+(add-hook 'c-mode-hook   #'electric-pair-mode)
+(add-hook 'c++-mode-hook #'electric-pair-mode)
 
 
 ;;; clang-format
