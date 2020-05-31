fe92b74baefbe01187844e4f310cf7cb1564a0c2
Henry Till <henrytill@gmail.com>
2020-05-31 18:57:49 -0400

    emacs: update feature-compile

--- emacs/.emacs.d/feature/feature-compile.el
+++ emacs/.emacs.d/feature/feature-compile.el
@@ -1,6 +1,11 @@
 (use-package compile
   :commands compile
   :init
+  (ht/comment
+    ;; https://stackoverflow.com/questions/4556368/compiling-c-with-emacs-on-windows-system
+    (add-to-list 'compilation-error-regexp-alist
+                 '(msvc "^[ \t]*\\([A-Za-z0-9\\.][^(]*\\.\\(cpp\\|c\\|h\\|hpp\\)\\)(\\([0-9]+\\)) *: +\\(error\\|fatal error\\|warning\\) C[0-9]+:" 1 3))
+    nil)
   ;; http://endlessparentheses.com/ansi-colors-in-the-compilation-buffer-output.html
   (require 'ansi-color)
   (defun ht/colorize-compilation-buffer ()
