4b2bd921fc5554519710dda4c04cf89f856406b1
Henry Till <henrytill@gmail.com>
2020-08-19 16:49:37 -0400

    emacs: update feature-lsp

--- emacs/.emacs.d/feature/feature-lsp.el
+++ emacs/.emacs.d/feature/feature-lsp.el
@@ -1,6 +1,8 @@
 (when (version<= "26.1" emacs-version)
   (use-package eglot
     :ensure t
-    :commands eglot))
+    :commands eglot
+    :init
+    (setq eldoc-echo-area-use-multiline-p nil)))
 
 (provide 'feature-lsp)
