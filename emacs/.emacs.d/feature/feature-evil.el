dbcebba35ffc14034e4d9a99298157b098dfb784
Henry Till <henrytill@gmail.com>
2020-08-26 16:30:35 -0400

    emacs: update evil config

--- emacs/.emacs.d/feature/feature-evil.el
+++ emacs/.emacs.d/feature/feature-evil.el
@@ -172,7 +172,8 @@ _l_: evil-avy-goto-line
 (use-package evil
   :ensure t
   :init
-  (setq evil-want-abbrev-expand-on-insert-exit nil)
+  (setq evil-want-abbrev-expand-on-insert-exit nil
+        evil-ex-search-case 'sensitive)
   :config
   (use-package evil-surround
     :ensure t
