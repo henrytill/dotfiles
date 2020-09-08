313b22ff92841437ae469d655d0489df6ae7e175
Henry Till <henrytill@gmail.com>
2020-09-08 14:32:01 -0400

    emacs: update feature-lsp

--- emacs/.emacs.d/feature/feature-lsp.el
+++ emacs/.emacs.d/feature/feature-lsp.el
@@ -1,8 +1,10 @@
 (when (version<= "26.1" emacs-version)
   (use-package eglot
     :ensure t
-    :commands eglot
     :init
-    (setq eldoc-echo-area-use-multiline-p nil)))
+    (setq eldoc-echo-area-use-multiline-p nil)
+    :config
+    (setq eglot-server-programs
+          (ht/replace-item-in-alist eglot-server-programs 'rust-mode '("rust-analyzer")))))
 
 (provide 'feature-lsp)
