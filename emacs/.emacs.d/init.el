eb151e336ae146b376c65710a51f1c8dcc2547a1
Henry Till <henrytill@gmail.com>
2022-05-03 15:54:18 -0700

    emacs: refactor whitespace config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -673,17 +673,6 @@ _s_: magit-status
 
 (use-package whitespace
   :commands whitespace-mode
-
-  :init
-  (setq whitespace-style '(face lines-tail trailing)
-        whitespace-line-column 100)
-
-  (defun ht/whitespace-mode ()
-    (when (derived-mode-p 'prog-mode)
-      (whitespace-mode 1)))
-
-  (add-hook 'hack-local-variables-hook 'ht/whitespace-mode)
-
   :config
   (defun ht/toggle-tabs-display ()
     (interactive)
@@ -707,9 +696,15 @@ _s_: magit-status
     (if (memq 'lines-tail whitespace-style)
         (setq whitespace-style (remove 'lines-tail whitespace-style))
       (add-to-list 'whitespace-style 'lines-tail))
-    (whitespace-mode 1))
+    (whitespace-mode 1)))
 
-  nil)
+(defun ht/whitespace-mode ()
+  (when (derived-mode-p 'prog-mode)
+    (setq whitespace-style '(face lines-tail trailing)
+          whitespace-line-column 100)
+    (whitespace-mode 1)))
+
+(add-hook 'hack-local-variables-hook 'ht/whitespace-mode)
 
 ;;; ATS ;;;
 
