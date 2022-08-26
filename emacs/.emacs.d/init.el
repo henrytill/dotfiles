33023168e397b2714d23ae24f908bbf6d4621222
Henry Till <henrytill@gmail.com>
2022-08-25 20:15:53 -0700

    emacs: remove c styles

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -421,20 +421,6 @@
 
 (add-hook 'c-mode-common-hook #'ht/modify-c-syntax-entries)
 
-(with-eval-after-load 'cc-styles
-  (c-add-style "bsd-tabs" '("bsd" (indent-tabs-mode . t)))
-  (c-add-style "hcpp"     '((indent-tabs-mode . nil)
-                            (c-basic-offset . 4)
-                            (c-offsets-alist (statement-block-intro . +)
-                                             (knr-argdecl-intro . 0)
-                                             (substatement-open . 0)
-                                             (substatement-label . 0)
-                                             (label . 0)
-                                             (statement-cont . +)
-                                             (innamespace . 0))))
-  (add-to-list 'c-default-style '(c-mode   . "bsd-tabs"))
-  (add-to-list 'c-default-style '(c++-mode . "hcpp")))
-
 (setq path-to-ctags "ctags")
 
 (defun ht/run-ctags (paths)
