881f9d841bc6fab042ee06580dd8ba24049f0336
Henry Till <henrytill@gmail.com>
2022-11-11 21:53:50 -0800

    emacs: add wayland copy/paste config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -814,6 +814,30 @@
 (add-hook 'comint-output-filter-functions #'comint-osc-process-output)
 
 
+;;; --- WAYLAND COPY/PASTE --- ;;;
+
+(when (and (getenv "WAYLAND_DISPLAY")
+           (executable-find "wl-copy")
+           (executable-find "wl-paste"))
+  (defvar wl-copy-process nil)
+
+  (defun wl-copy (text)
+    (setq wl-copy-process (make-process :name "wl-copy"
+                                        :buffer nil
+                                        :command '("wl-copy" "-f" "-n")
+                                        :connection-type 'pipe))
+    (process-send-string wl-copy-process text)
+    (process-send-eof wl-copy-process))
+
+  (defun wl-paste ()
+    (if (and wl-copy-process (process-live-p wl-copy-process))
+        nil
+      (shell-command-to-string "wl-paste -n | tr -d \r")))
+
+  (setq interprogram-cut-function 'wl-copy
+        interprogram-paste-function 'wl-paste))
+
+
 ;;; --- POSTLUDE --- ;;;
 
 (defvar ht/clang-format-on-save nil)
