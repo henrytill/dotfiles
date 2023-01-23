9010e45ea04bce43736e3c8d85a9cb531a3aa8b5
Henry Till <henrytill@gmail.com>
2023-01-23 14:41:57 -0800

    emacs: update auctex config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -768,18 +768,17 @@
 ;;; TEX
 
 (use-package auctex
-  :mode ("\\.tex\\'" . TeX-latex-mode)
-  :config
-  (setq TeX-auto-save t
-        TeX-parse-self t)
-  (setq-default TeX-master nil))
+  :ensure t
+  :hook ((LaTeX-mode . display-line-numbers-mode)))
 
 (with-eval-after-load 'tex
-  (when (and (is-linux-p) (executable-find "mupdf-x11"))
+  (setq TeX-auto-save t
+        TeX-parse-self t)
+  (when (and (is-linux-p) (executable-find "zathura"))
     (add-to-list 'TeX-view-program-list
-                 '("mupdf" ("mupdf-x11" (mode-io-correlate " -p %(outpage)") " %o")))
+                 '("zathura" ("zathura" (mode-io-correlate " -P %(outpage)") " %o")))
     (add-to-list 'TeX-view-program-selection
-                 '(output-pdf "mupdf"))))
+                 '(output-pdf "zathura"))))
 
 (defun ht/bibtex-mode ()
   (ht/truncate-lines)
