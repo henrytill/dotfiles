95c5b248c5d175d6ac09e57928b4d56cbdb175ae
Henry Till <henrytill@gmail.com>
2023-03-25 09:21:54 -0700

    install.sh: tweak

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 echo "dotfiles: ${DIR}"
 
-if [ -n "$IN_DEVCONTAINER" ]; then
+if [ -n "$IN_DEV_CONTAINER" ]; then
     echo "In Dev Container, exiting..."
     exit 0
 fi
