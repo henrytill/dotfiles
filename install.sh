78051405c68b5772e6f5bdb90f36440ddad19441
Henry Till <henrytill@gmail.com>
2023-03-25 09:17:13 -0700

    install.sh: update

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
-echo "dotfiles directory: ${DIR}"
+echo "dotfiles: ${DIR}"
+
+if [ -n "$IN_DEVCONTAINER" ]; then
+    echo "In Dev Container, exiting..."
+    exit 0
+fi
 
 make -C $DIR
