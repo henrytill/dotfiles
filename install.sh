2c12b5ea9ff447c81febbbb4d01ea3a05e02c083
Henry Till <henrytill@gmail.com>
2023-03-25 22:16:11 +0000

    install.sh: update

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
-echo "dotfiles: ${DIR}"
+echo "DIR=${DIR}"
 
 if [ -n "$IN_DEV_CONTAINER" ]; then
-    echo "In Dev Container, exiting..."
+    echo "IN_DEV_CONTAINER=${IN_DEV_CONTAINER}"
+    ln -frsv $DIR/git/.gitignore_global $DIR/..
     exit 0
 fi
 
