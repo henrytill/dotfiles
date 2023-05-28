7f37a5192d56e7843c6f5ca16e9dd3637c908586
Henry Till <henrytill@gmail.com>
2023-05-28 15:54:39 -0700

    emacs: add custom.el

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(elpher yaml-mode rec-mode markdown-mode dockerfile-mode debbugs auctex sml-mode rust-mode ediprolog tuareg nix-mode lua-mode sly geiser-racket geiser-guile cmake-mode paredit magit macrostep haskell-mode go-mode gnu-apl-mode geiser forth-mode eglot company bison-mode))
+ '(safe-local-variable-values '((ht/clang-format-on-save . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-constant-face ((t nil)))
+ '(font-lock-function-name-face ((t nil)))
+ '(font-lock-keyword-face ((t nil)))
+ '(font-lock-type-face ((t nil)))
+ '(font-lock-variable-name-face ((t nil)))
+ '(link ((t (:underline t))))
+ '(tuareg-font-double-semicolon-face ((t nil))))
