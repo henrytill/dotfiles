1001e67915ddc52bb96b80ae8cb06acdf0e8cee4
Henry Till <henrytill@gmail.com>
2024-03-18 19:35:34 -0700

    emacs: update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -12,6 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-lock-builtin-face ((t nil)))
  '(font-lock-constant-face ((t nil)))
  '(font-lock-function-name-face ((t nil)))
  '(font-lock-keyword-face ((t nil)))
