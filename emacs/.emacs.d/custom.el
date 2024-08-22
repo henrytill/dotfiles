9017e06d87c09816a2359c4e2873663a55b93bd1
Henry Till <henrytill@gmail.com>
2024-08-21 19:43:33 -0700

    emacs: update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -14,6 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(agda2-highlight-keyword-face ((t nil)))
  '(font-lock-builtin-face ((t nil)))
  '(font-lock-constant-face ((t nil)))
  '(font-lock-function-name-face ((t nil)))
