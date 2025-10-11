8465fd35e47b760366b67b8e3a10cd4d63ec5722
Henry Till <henrytill@gmail.com>
2025-10-10 21:52:59 -0700

    emacs: update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -27,10 +27,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(compilation-error ((t nil)))
- '(compilation-info ((t nil)))
- '(compilation-warning ((t nil)))
- '(eglot-mode-line ((t (:weight bold))))
- '(flymake-error-echo ((t nil)))
- '(flymake-note-echo ((t nil)))
- '(flymake-warning-echo ((t nil))))
+ )
