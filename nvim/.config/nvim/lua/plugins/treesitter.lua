9d5c4770a75a471ccd3646d3f697d2af9435b0c8
Henry Till <henrytill@gmail.com>
2025-11-04 13:40:31 -0800

    Revert "nvim: remove nvim-treesitter{,-textobjects}"
    This reverts commit fc9c2e3b6955a78381f5d31448aa018a953224ee.

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,48 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+				"go",
+				"rust",
+				"ocaml",
+			},
+
+			sync_install = false,
+
+			auto_install = false,
+
+			ignore_install = {},
+
+			modules = {},
+
+			highlight = {
+				enable = false,
+			},
+
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
+				},
+			},
+
+			indent = {
+				enable = true,
+			},
+		})
+	end,
+}
