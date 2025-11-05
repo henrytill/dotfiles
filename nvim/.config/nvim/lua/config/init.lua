4e64318652a05142dec861208b937a848aeaa490
Henry Till <henrytill@gmail.com>
2025-11-04 19:45:00 -0800

    nvim: inline folds.scm

--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -182,14 +182,60 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
--- Load custom C fold queries from file
-local c_folds_path = vim.fn.stdpath("config") .. "/queries/c/folds.scm"
-local c_folds_file = io.open(c_folds_path, "r")
-if c_folds_file then
-	local c_folds_content = c_folds_file:read("*all")
-	c_folds_file:close()
-	vim.treesitter.query.set("c", "folds", c_folds_content)
-end
+-- Custom C fold queries - fold bodies only, keeping declarations/conditions visible
+vim.treesitter.query.set(
+	"c",
+	"folds",
+	[[
+; Function bodies (not the signature)
+(function_definition
+  body: (compound_statement) @fold)
+
+; Control flow statement bodies (not the condition/header)
+(if_statement
+  consequence: (compound_statement) @fold)
+
+(else_clause
+  (compound_statement) @fold)
+
+(for_statement
+  body: (compound_statement) @fold)
+
+(while_statement
+  body: (compound_statement) @fold)
+
+(do_statement
+  body: (compound_statement) @fold)
+
+(switch_statement
+  body: (compound_statement) @fold)
+
+; Struct and enum bodies (not the declaration)
+(struct_specifier
+  body: (field_declaration_list) @fold)
+
+(enum_specifier
+  body: (enumerator_list) @fold)
+
+; These fold the entire construct (no separate body to target)
+[
+  (case_statement)
+  (comment)
+  (preproc_if)
+  (preproc_elif)
+  (preproc_else)
+  (preproc_ifdef)
+  (preproc_function_def)
+  (initializer_list)
+  (gnu_asm_expression)
+  (preproc_include)+
+] @fold
+
+; Nested compound statements
+(compound_statement
+  (compound_statement) @fold)
+]]
+)
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "c", "go", "rust", "lua", "ocaml", "haskell" },
