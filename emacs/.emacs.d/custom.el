41ebb9977cffde38183ee529c0e3ae205ba30182
Henry Till <henrytill@gmail.com>
2026-05-31 20:48:09 -0700

    emacs: use unifont for gui

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -31,4 +31,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Unifont" :foundry "GNU " :slant normal :weight regular :height 120 :width normal)))))
